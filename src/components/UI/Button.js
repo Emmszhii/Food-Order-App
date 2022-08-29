a7a683954688a564ee9dc1e0f8950839f2cc2f13
Emmszhii <emmanuelmanalo707@gmail.com>
2022-08-29 14:35:51 +0800

    Adding HeaderButton Component

---
+++ src/components/UI/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Button.module.css';
+
+const Button = (props) => {
+  return (
+    <div className={`${classes.button} ${props.className}`}>
+      {props.children}
+    </div>
+  );
+};
+
+export default Button;
