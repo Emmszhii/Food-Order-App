3099905572d0dc5a3de068ee07d85ca42b78d765
Emmszhii <emmanuelmanalo707@gmail.com>
2022-08-29 16:22:19 +0800

    Adding Meal Item Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meal';
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
