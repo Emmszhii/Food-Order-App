bb74ee48ee982b6e2f023695bc8beabf0db5db8b
Emmszhii <emmanuelmanalo707@gmail.com>
2022-08-29 17:06:32 +0800

    Adding a Form Input Component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import classes from './MealItem.module.css';
 
+import MealItemForm from './MealItemForm';
+
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
 
@@ -11,7 +13,9 @@ const MealItem = (props) => {
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
-        <div></div>
+      </div>
+      <div>
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
